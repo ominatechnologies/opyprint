03c11342065b9f9fc9c6e26ea4c7071f303d251c
Wouter Van den Broeck <wouter@addith.be>
2020-01-06 19:05:32 +0100

    feat: The 'print' function now also accepts the 'bullet' parameter.

--- wpyprint/print.py
+++ wpyprint/print.py
@@ -6,7 +6,7 @@ _py_print = print
 _ppc = PPContext()
 
 
-def print(*args) -> None:
+def print(*args, bullet: str = None) -> None:
     """
     A replacement for the standard Python print function that pretty-prints
     the given arguments.
@@ -19,8 +19,12 @@ def print(*args) -> None:
         :attr:`wpyprint.PPContext.print_name_value_pairs` attribute is true,
         then the arguments are printed as a name-value pair (in a dict).
         Otherwise multiple arguments are printed as list.
+    :param bullet: When given, prefix the formatted result with a bullet,
+        when this is not yet the case. The argument may be either a
+        non-empty string, the first character of which is taken
+        as the bullet, or true to use the current or default bullet.
     """
     if len(args) == 0:
         _py_print()
     else:
-        _py_print(_ppc.format(*args))
+        _py_print(_ppc.format(*args, bullet=bullet))
