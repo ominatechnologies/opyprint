f2f994c633e9d1ea26d3e0efed30fbe486d04e12
Wouter Van den Broeck <wouter@addith.be>
2020-01-07 07:57:47 +0100

    feat: The 'print' function now also accepts the 'width' parameter.

--- wpyprint/print.py
+++ wpyprint/print.py
@@ -3,10 +3,11 @@ from __future__ import annotations
 from .PPContext import PPContext
 
 _py_print = print
-_ppc = PPContext()
 
 
-def print(*args, bullet: str = None) -> None:
+def print(*args,
+          bullet: str = None,
+          width: int = PPContext.default_width) -> None:
     """
     A replacement for the standard Python print function that pretty-prints
     the given arguments.
@@ -23,8 +24,11 @@ def print(*args, bullet: str = None) -> None:
         when this is not yet the case. The argument may be either a
         non-empty string, the first character of which is taken
         as the bullet, or true to use the current or default bullet.
+    :param width: Total width in characters, including bullets and
+        indentation. Defaults to the value of the 'default_width' class
+        attribute of the :class:`~wpyprint.PPContext.PPContext` class.
     """
     if len(args) == 0:
         _py_print()
     else:
-        _py_print(_ppc.format(*args, bullet=bullet))
+        PPContext(width=width).print(*args, bullet=bullet)
