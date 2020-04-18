59e1ddf47bd0ee4f5982f117623e11f274054a7b
Wouter Van den Broeck <wouter@addith.be>
2020-04-18 10:35:08 +0200

    style: format

--- opyprint/__init__.py
+++ opyprint/__init__.py
@@ -5,7 +5,7 @@ from .typing import StyleOptions
 from .utils import (dict_lt, is_dict, is_multiliner, is_oneliner, is_set,
                     is_tuple, lt)
 
-__all__ = [
+__all__ = (
     'PPContext',
     'PPStyles',
     'StyleOptions',
@@ -17,4 +17,4 @@ __all__ = [
     'is_tuple',
     'lt',
     'print',
-]
+)
--- opyprint/utils/__init__.py
+++ opyprint/utils/__init__.py
@@ -2,7 +2,7 @@ from .lt import dict_lt, lt
 from .predicates import (is_bullettable, is_dict, is_multiliner, is_oneliner,
                          is_set, is_tuple)
 
-__all__ = [
+__all__ = (
     'dict_lt',
     'is_bullettable',
     'is_dict',
@@ -11,4 +11,4 @@ __all__ = [
     'is_set',
     'is_tuple',
     'lt',
-]
+)
