c9517ddf177071b07bb91ad04a9d7b4f4e1a1cd7
Wouter Van den Broeck <wouter@addith.be>
2020-07-31 13:59:59 +0200

    style: formatting

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 # Single-sourced project configuration values:
 
 # the full version, including alpha/beta/rc tags
-release = '0.12.0'
+release = "0.12.0"
 
 # the short X.Y version
-version = '0.12'
+version = "0.12"
 
 # distribution package name
-name = 'opyprint'
+name = "opyprint"
 
 # capitalized label
-project = 'OPYPRINT'
+project = "OPYPRINT"
 
 description = "An opinionated pretty-print library for Python."
 author = "Wouter Van den Broeck"
--- setup.py
+++ setup.py
@@ -4,48 +4,48 @@ from config import author, author_email, description, name, release, repo
 
 
 def readme():
-    with open('README.rst') as f:
+    with open("README.rst") as f:
         return f.read()
 
 
 # 3rd-party run-time requirements:
 install_requires = [
-    'colorama==0.4.3',
-    'typing-extensions==3.7.4.2',
+    "colorama==0.4.3",
+    "typing-extensions==3.7.4.2",
 ]
 
 # 1st-party run-time requirements:
 install_requires += [
-    'frozendict @ git+https://github.com/ominatechnologies/'
-    'frozendict.git@1.2.3#egg=frozendict',
+    "frozendict @ git+https://github.com/ominatechnologies/"
+    "frozendict.git@1.2.3#egg=frozendict",
 ]
 
 setup(
     author=author,
     author_email=author_email,
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
     ],
     data_files=[
-        ('', ['AUTHORS.rst', 'CHANGELOG.rst', 'LICENSE', 'README.rst'])
+        ("", ["AUTHORS.rst", "CHANGELOG.rst", "LICENSE", "README.rst"])
     ],
     description=description,
     include_package_data=True,
     install_requires=install_requires,
     keywords=[
-        'pretty-printing',
-        'python',
-        'utilities',
+        "pretty-printing",
+        "python",
+        "utilities",
     ],
-    license='MIT',
+    license="MIT",
     long_description=readme(),
     name=name,
     package_data={
-        name: ['py.typed'],
+        name: ["py.typed"],
     },
     packages=find_packages(),
-    python_requires='>=3.7',
+    python_requires=">=3.7",
     url=repo,
     version=release,
     zip_safe=False,
